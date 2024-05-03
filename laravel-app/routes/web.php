0b74c5ec569207985d072821bbc0a1ffe94d1823
aldoawp <aristaaldo2@gmail.com>
2024-05-04 00:08:37 +0700

    ft: add login admin view

--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -6,10 +6,23 @@ Route::get('/', function() {
     return view('dashboard');
 });
 
-Route::get('/login', function() {
-    return view('login');
+Route::get('/login-user', function() {
+    return view('login-user');
+});
+
+Route::get('/login-admin', function() {
+    return view('login-admin');
 });
 
 Route::get('/register', function() {
     return view('register');
 });
+
+Route::get('/attendance', function() {
+    return view('attendance');
+});
+
+Route::get('/employees', function() {
+    return view('employees');
+});
+
