8bc01a65fb14525f2ab8c0a2945bd0a6ea4c3c9a
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-05-05 09:53:23 +0700

    add db col: att <- type

--- laravel-app/database/migrations/2024_05_02_141602_attendances.php
+++ laravel-app/database/migrations/2024_05_02_141602_attendances.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('attendances', function (Blueprint $table) {
             $table->integer('User_ID')->unsigned();
             $table->timestamp('Date_Time')->default(now());
+            $table->enum('Type', ['Check-In', 'Check-Out']);
 
             $table->foreign('User_ID')->references('User_ID')->on('users')->onDelete('cascade');
         });
