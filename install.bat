f4deb337a790a4f1d78cf4423b7694047de9d7d2
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-05-05 13:33:20 +0700

    Update installation cmd

--- install.bat
+++ install.bat
@@ -1,10 +1,10 @@
 @echo off
 
-cd ./laravel-app
-
 :: Frontend
-start composer install && copy .env.example .env && php artisan key:generate && php artisan migrate && npm install && echo Frontend Installation Done..
+start cd laravel-app && composer install && copy .env.example .env 
+    && php artisan key:generate && php artisan migrate && npm install 
+    && echo Frontend Installation Done..
 
 :: Backend
-cd ../server
-start copy .env.example .env && echo Backend Installation Done..
+start cd server && copy .env.example .env && cd utils && go build ./addAdmin.go 
+    && echo Backend Installation Done..
