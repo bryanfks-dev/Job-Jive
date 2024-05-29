386be5b38523ce1f5bfd1db409bfb335aa267b73
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-05-29 22:18:06 +0700

    refactor codes

--- laravel-app/app/Http/Controllers/UserLoginController.php
+++ laravel-app/app/Http/Controllers/UserLoginController.php
@@ -21,25 +21,34 @@ class UserLoginController extends Controller
 
         $remember = $request->input('remember') === "on";
 
-        // Send request to be server
-        $response =
-            Http::post('http://127.0.0.1:5000/auth/user/login', array_merge(
-                $credentials, ['remember' => $remember,]));
+        $credentials['remember'] = $remember;
 
-        switch ($response['status']) {
-            case 401:
-                return redirect()->back();
+        try {
+            // Send request to be server
+            $response =
+            Http::post('http://127.0.0.1:5000/auth/user/login', $credentials);
 
-            case 200:
-                // Create session token
-                session(['token' => $response['token']]);
+            if ($response->successful()) {
+                switch ($response['status']) {
+                    case 401:
+                        return redirect()->back();
 
-                // Create cookie
-                if ($remember) {
-                    cookie('token', $response['token'], 60 * 24 * 7); // 7 days expire time
+                    case 200:
+                        // Create session token
+                        session(['token' => $response['token']]);
+
+                        // Create cookie
+                        if ($remember) {
+                            cookie('token', $response['token'], 60 * 24 * 7); // 7 days expire time
+                        }
+
+                        return redirect()->intended(route('user.dashboard'));
                 }
-
-                return redirect()->intended(route('user.dashboard'));
+            } else {
+                return redirect()->back();
+            }
+        } catch (\Exception $e) {
+            return redirect()->back();
         }
     }
 }
