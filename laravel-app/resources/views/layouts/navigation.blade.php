b9eef269b6ebc4b40d9c71333acfef804e14c44c
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-05-29 20:55:47 +0700

    fix user dashboard route

--- laravel-app/resources/views/layouts/navigation.blade.php
+++ laravel-app/resources/views/layouts/navigation.blade.php
@@ -105,7 +105,7 @@
       <ul class="space-y-2">
         <li>
           <a
-            href="{{ route('dashboard') }}"
+            href="{{ route('user.dashboard') }}"
             class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
           >
             <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor">
