7ebe3b76f732f3fbf938d0dc234761c8a14bb9a6
aldoawp <aristaaldo2@gmail.com>
2024-05-30 23:40:58 +0700

    style: change text content
    to give better context

--- laravel-app/resources/views/admin/employee.blade.php
+++ laravel-app/resources/views/admin/employee.blade.php
@@ -337,7 +337,7 @@
                                 <div class="col-span-2 sm:col-span-1">
                                     <label for="email"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                                    <input type="text" name="email" id="email"
+                                    <input type="email" name="email" id="email"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" required="">
                                 </div>
                                 <div class="col-span-2 sm:col-span-1">
@@ -420,7 +420,7 @@
                                     stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
                             <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to
-                                delete this product?</h3>
+                                delete this employee?</h3>
                             <button data-modal-hide="popup-modal" type="button"
                                 class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                                 Yes, I'm sure
