636fc2f3ce8c2575e694dbed5e28205a996bbbc9
aldoawp <aristaaldo2@gmail.com>
2024-05-31 16:37:54 +0700

    ft: admin config view

--- laravel-app/resources/views/admin/config.blade.php
+++ laravel-app/resources/views/admin/config.blade.php
@@ -1,5 +1,51 @@
 @extends('layouts.admin-app')
 
 @section('content')
-    
+
+<div class="flex flex-col gap-4">
+    <div class="grid gap-4 mb-4 grid-cols-2">
+        <div class="col-span-2 sm:col-span-1">
+            <label for="checkin_time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Check-In Time</label>
+            <input type="time" name="checkin_time" id="checkin_time"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="ex. Sales" required="">
+        </div>
+        <div class="col-span-2 sm:col-span-1">
+            <label for="checkout_hour" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Check-Out Time</label>
+            <input type="time" name="checkout_hour" id="checkout_hour"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="ex. Sales" required="">
+        </div>
+        <div class="col-span-2 sm:col-span-1">
+            <label for="absence_quota" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Absence Quota</label>
+            <input type="number" name="absence_quota" id="absence_quota"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                required="" min="0">
+        </div>
+        <div class="col-span-2 sm:col-span-1">
+            <label for="daily_work_hours" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Daily Work Hours</label>
+            <input type="number" name="daily_work_hours" id="daily_work_hours"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                required="" min="0">
+        </div>
+        <div class="col-span-2 sm:col-span-1">
+            <label for="weekly_work_hours" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Weekly Work Hours</label>
+            <input type="number" name="weekly_work_hours" id="weekly_work_hours"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                required="" min="0">
+        </div>
+    </div>
+
+    <div class="mb-4">
+        <button type="submit" class="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
+            Submit Config
+        </button>
+    </div>
+</div>
+
 @endsection
--- laravel-app/resources/views/partials/user/employees/employee-card.blade.php
+++ laravel-app/resources/views/partials/user/employees/employee-card.blade.php
@@ -31,13 +31,37 @@
         <span
             class="mb-2 bg-green-100 text-green-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-800">Employee</span>
         <h5 class="mb-1 text-center text-xl font-medium text-gray-900 dark:text-white">Cinta Kuswandari</h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">cintakuswandari2@gmail.com</span>
+        <span class="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400">
+            <svg xmlns="http://www.w3.org/2000/svg" class="fill-blue-500" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L4.4 8.25a.85.85 0 1 1 .9-1.44L12 11l6.7-4.19a.85.85 0 1 1 .9 1.44"/></svg>
+            cintakuswandari2@gmail.com
+        </span>
+        <span class="flex items-center gap-1 text-sm text-gray-500 dark:text-gray-400">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 256 258">
+                <defs>
+                    <linearGradient id="logosWhatsappIcon0" x1="50%" x2="50%" y1="100%" y2="0%">
+                        <stop offset="0%" stop-color="#1faf38" />
+                        <stop offset="100%" stop-color="#60d669" />
+                    </linearGradient>
+                    <linearGradient id="logosWhatsappIcon1" x1="50%" x2="50%" y1="100%" y2="0%">
+                        <stop offset="0%" stop-color="#f9f9f9" />
+                        <stop offset="100%" stop-color="#fff" />
+                    </linearGradient>
+                </defs>
+                <path fill="url(#logosWhatsappIcon0)"
+                    d="M5.463 127.456c-.006 21.677 5.658 42.843 16.428 61.499L4.433 252.697l65.232-17.104a122.994 122.994 0 0 0 58.8 14.97h.054c67.815 0 123.018-55.183 123.047-123.01c.013-32.867-12.775-63.773-36.009-87.025c-23.23-23.25-54.125-36.061-87.043-36.076c-67.823 0-123.022 55.18-123.05 123.004" />
+                <path fill="url(#logosWhatsappIcon1)"
+                    d="M1.07 127.416c-.007 22.457 5.86 44.38 17.014 63.704L0 257.147l67.571-17.717c18.618 10.151 39.58 15.503 60.91 15.511h.055c70.248 0 127.434-57.168 127.464-127.423c.012-34.048-13.236-66.065-37.3-90.15C194.633 13.286 162.633.014 128.536 0C58.276 0 1.099 57.16 1.071 127.416m40.24 60.376l-2.523-4.005c-10.606-16.864-16.204-36.352-16.196-56.363C22.614 69.029 70.138 21.52 128.576 21.52c28.3.012 54.896 11.044 74.9 31.06c20.003 20.018 31.01 46.628 31.003 74.93c-.026 58.395-47.551 105.91-105.943 105.91h-.042c-19.013-.01-37.66-5.116-53.922-14.765l-3.87-2.295l-40.098 10.513z" />
+                <path fill="#fff"
+                    d="M96.678 74.148c-2.386-5.303-4.897-5.41-7.166-5.503c-1.858-.08-3.982-.074-6.104-.074c-2.124 0-5.575.799-8.492 3.984c-2.92 3.188-11.148 10.892-11.148 26.561c0 15.67 11.413 30.813 13.004 32.94c1.593 2.123 22.033 35.307 54.405 48.073c26.904 10.609 32.379 8.499 38.218 7.967c5.84-.53 18.844-7.702 21.497-15.139c2.655-7.436 2.655-13.81 1.859-15.142c-.796-1.327-2.92-2.124-6.105-3.716c-3.186-1.593-18.844-9.298-21.763-10.361c-2.92-1.062-5.043-1.592-7.167 1.597c-2.124 3.184-8.223 10.356-10.082 12.48c-1.857 2.129-3.716 2.394-6.9.801c-3.187-1.598-13.444-4.957-25.613-15.806c-9.468-8.442-15.86-18.867-17.718-22.056c-1.858-3.184-.199-4.91 1.398-6.497c1.431-1.427 3.186-3.719 4.78-5.578c1.588-1.86 2.118-3.187 3.18-5.311c1.063-2.126.531-3.986-.264-5.579c-.798-1.593-6.987-17.343-9.819-23.64" />
+            </svg>
+            081234567890
+        </span>
 
         <div class="flex mt-4 md:mt-6">
-            <a href="#"
-                class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Contact</a>
-            <a href="/employees/view"
-                class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Edit</a>
+            <div
+                class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 cursor-pointer" data-modal-target="update-modal" data-modal-toggle="update-modal">Edit</div>
         </div>
+
+        @include("partials.admin.employees.update-modal")
     </div>
 </div>
