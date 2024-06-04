c72d7efaeeca3c598fc40dab9fc4ce2fee39f4ca
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-06-04 11:54:28 +0700

    reformat project structure and add salary model

--- go-worker/cron_job/main.go
+++ go-worker/cron_job/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"db"
 	"log"
+	"models"
 
 	"github.com/robfig/cron"
 )
 
 func main() {
 	cron := cron.New()
-	err := cron.AddFunc("*/1 * * * *", func() {
-		fmt.Println("Wokrs")
+	err := cron.AddFunc("0 0 0 1 * *", func() {
+		tx, err := db.Conn.Begin()
+
+		defer tx.Rollback()
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		err = models.ResetCurrentSalary()
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		if err := tx.Rollback(); err != nil {
+			panic(err.Error())
+		}
 	})
 
 	if err != nil {
