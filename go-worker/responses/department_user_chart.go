e8e44f216c9c1c63285ffb12815a9b45094a9320
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-06-21 12:49:33 +0700

    fix absence count query

--- go-worker/responses/department_user_chart.go
+++ go-worker/responses/department_user_chart.go
@@ -8,12 +8,12 @@ type DepartmentUserChartResponse struct {
 }
 
 func (department_user_chart_response *DepartmentUserChartResponse) GetDepartmentUsersAttendance(today string, department_id int, manager_id int) (int, error) {
-	stmt := "SELECT COUNT(*), COALESCE(SUM(a.Type = 'Check-Out'), 0) FROM `users` u LEFT JOIN `attendances` a ON a.User_ID = u.User_ID WHERE DATE(a.Date_Time) = ? AND u.Department_ID = ? AND u.User_ID <> ?"
+	stmt := "SELECT (SELECT COUNT(*) FROM `users` us WHERE us.Department_ID = ? AND us.User_ID <> ?), COALESCE(SUM(a.Type = 'Check-Out'), 0) FROM `users` u LEFT JOIN `attendances` a ON a.User_ID = u.User_ID WHERE DATE(a.Date_Time) = ? AND u.Department_ID = ? AND u.User_ID <> ?"
 
 	employee_count := 0
 
 	err :=
-		db.Conn.QueryRow(stmt, today, department_id, manager_id).
+		db.Conn.QueryRow(stmt, department_id, manager_id, today, department_id, manager_id).
 			Scan(&employee_count, &department_user_chart_response.AttendCount)
 
 	if err != nil {
@@ -24,12 +24,12 @@ func (department_user_chart_response *DepartmentUserChartResponse) GetDepartment
 }
 
 func (department_user_chart_response *DepartmentUserChartResponse) GetDepartmentUsersAttendanceBetween(start_date string, end_date string, department_id int, manager_id int) (int, error) {
-	stmt := "SELECT COUNT(*), COALESCE(SUM(a.Type = 'Check-Out'), 0) FROM `users` u LEFT JOIN `attendances` a ON a.User_ID = u.User_ID WHERE (DATE(a.Date_Time) >= ? AND DATE(a.Date_Time) <= ?) AND u.Department_ID = ? AND u.User_ID <> ?"
+	stmt := "SELECT (SELECT COUNT(*) FROM `users` us WHERE us.Department_ID = ? AND us.User_ID <> ?), COALESCE(SUM(a.Type = 'Check-Out'), 0) FROM `users` u LEFT JOIN `attendances` a ON a.User_ID = u.User_ID WHERE (DATE(a.Date_Time) >= ? && DATE(a.Date_Time) <= ?) AND u.Department_ID = ? AND u.User_ID <> ?"
 
 	employee_count := 0
 
 	err :=
-		db.Conn.QueryRow(stmt, start_date, end_date, department_id, manager_id).
+		db.Conn.QueryRow(stmt, department_id, manager_id, start_date, end_date, department_id, manager_id).
 			Scan(&employee_count, &department_user_chart_response.AttendCount)
 
 	if err != nil {
